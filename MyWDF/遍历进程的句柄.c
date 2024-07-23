e81f5480643497eedee6bbc7c61ba7408421359e
周腾 <826409843@qq.com>
2024-07-23 16:03:51 +0800

    可以查看句柄附加的进程id了

--- MyWDF/遍历进程的句柄.c
+++ MyWDF/遍历进程的句柄.c
@@ -1,31 +1,51 @@
 #include<ntifs.h>
 #include "win10结构体.h"
 
-#define TYPE_INDEX_OFFSET 0x18
-#define OB_HEADER_COOKIE 0x21
-#define PROCESS_TYPE 7
 
 
-typedef PHANDLE_TABLE_ENTRY(*PExpLookupHandleTableEntry)(
-	IN PHANDLE_TABLE HandleTable,//参数1是句柄表的地址，即 HandleTable，注意，这里 HandleTable 的低位不能清要，函数里要判断句柄表结构的
-	IN EXHANDLE handle//参数2是句柄值，PID 的值就是一个句柄值，调用 0perproces 打开一个进程得到的也是句柄值，前者用来索引全局句柄表，后者用来索引进程的句柯表。
-	);
+//windows匿名函数
+UINT_PTR ObGetObjectType(PVOID object);
 
+NTSTATUS ZwQueryInformationProcess(
+	IN HANDLE ProcessHandle, // 进程句柄
+	IN PROCESSINFOCLASS InformationClass, // 信息类型
+	OUT PVOID ProcessInformation, // 缓冲指针
+	IN ULONG ProcessInformationLength, // 以字节为单位的缓冲大小
+	OUT PULONG ReturnLength OPTIONAL // 写入缓冲的字节数
+);
 
-BOOLEAN IsProcess(PVOID64 Address)
+//typedef PHANDLE_TABLE_ENTRY(*PExpLookupHandleTableEntry)(
+//	IN PHANDLE_TABLE HandleTable,//参数1是句柄表的地址，即 HandleTable，注意，这里 HandleTable 的低位不能清要，函数里要判断句柄表结构的
+//	IN EXHANDLE handle//参数2是句柄值，PID 的值就是一个句柄值，调用 0perproces 打开一个进程得到的也是句柄值，前者用来索引全局句柄表，后者用来索引进程的句柯表。
+//	);
+
+//进程句柄转PID
+UINT32 HandleToPid(IN HANDLE ProcessID, IN HANDLE handle)
 {
-	UINT8 uTypeIndex;
-	UINT8 uByte;
+	KAPC_STATE apc_state;
+	PEPROCESS pEProcess = 0;
+	NTSTATUS status;
+	PROCESS_BASIC_INFORMATION pbi = { 0 };
 
-	uByte = ((ULONG64)Address >> 8) & 0xff;
-	uTypeIndex = *(PCHAR)((PCHAR)Address + TYPE_INDEX_OFFSET);
-	uTypeIndex = uTypeIndex ^ OB_HEADER_COOKIE ^ uByte;
-
-	if (uTypeIndex == PROCESS_TYPE) {
-		return TRUE;
+	RtlZeroMemory(&apc_state, sizeof(KAPC_STATE));
+	status = PsLookupProcessByProcessId(ProcessID, &pEProcess);
+	if (!NT_SUCCESS(status))
+		return 0;
+	//切换进程空间
+	KeStackAttachProcess((PRKPROCESS)pEProcess, &apc_state);
+	//在已切换的进程中，查看这个句柄
+	status = ZwQueryInformationProcess(handle,
+		ProcessBasicInformation,
+		(PVOID)&pbi,
+		sizeof(PROCESS_BASIC_INFORMATION),
+		NULL);
+	//分离线程
+	KeUnstackDetachProcess(&apc_state);
+	if (NT_SUCCESS(status))
+	{
+		return (UINT32)pbi.UniqueProcessId;
 	}
-
-	return FALSE;
+	return 0;
 }
 
 //合法返回TRUE，否则返回FALSE
@@ -46,14 +66,14 @@ ULONG64 HandleEntryTable2ObjectHeader(PHANDLE_TABLE_ENTRY addr)
 	return ((addr->name1.LowValue >> 0x10) & 0xFFFFFFFFFFFFFFF0) + 0xFFFF000000000000;
 }
 
-UINT_PTR ObGetObjectType(PVOID object);
+
 
 
 void 遍历指定进提所有句柄_WIN10(HANDLE ProcessId)
 {
 	NTSTATUS status;
-	PEPROCESS pProcess = 0;
-	status = PsLookupProcessByProcessId(ProcessId, &pProcess);
+	PEPROCESS pEProcess = 0;
+	status = PsLookupProcessByProcessId(ProcessId, &pEProcess);
 	if (!NT_SUCCESS(status))
 		return;
 
@@ -61,7 +81,7 @@ void 遍历指定进提所有句柄_WIN10(HANDLE ProcessId)
 
 
 	//指针指向HANDLE_TABLE
-	UINT_PTR pProcessHandleTable = (UINT_PTR)pProcess + Win10_EPROCESS_HANDLE_TABLE_OFFSET;
+	UINT_PTR pProcessHandleTable = (UINT_PTR)pEProcess + Win10_EPROCESS_HANDLE_TABLE_OFFSET;
 	UINT_PTR handleTable = RP(pProcessHandleTable);
 
 
@@ -72,6 +92,7 @@ void 遍历指定进提所有句柄_WIN10(HANDLE ProcessId)
 		KdPrint(("驱动:SYS handleCount=%X   +++++++>>>>>>>>>\n\n", count));
 		PVOID object = NULL;
 		POBJECT_TYPE objectType = NULL;
+		PWCH type = NULL;
 		UINT32 error = 0;
 		for (UINT32 i = 1; i <= cs; i++) {
 			UINT_PTR handle = i * 4;//句柄
@@ -110,53 +131,27 @@ void 遍历指定进提所有句柄_WIN10(HANDLE ProcessId)
 			objectType = (POBJECT_TYPE)ObGetObjectType(object);
 			if (objectType == NULL)
 			{
-				KdPrint(("Handle: 0x%llX, Object Type: 0  object: 0 \n\n", handle));
+				KdPrint(("句柄: 0x%llX, info=%p  Object Type: 0  object: 0 \n", handle, info));
 				continue;
 			}
-			UNICODE_STRING* typename = &objectType->Name;
-			KdPrint(("Handle: 0x%llX, Object Type: %S  object: %p \n\n", handle, typename->Buffer, object));
+			
+			type = objectType->Name.Buffer;
+			KdPrint(("句柄: 0x%llX,  info=%p  Object Type: %S  object: %p \n", handle, info, type, object));
+
+			if (type && _wcsicmp(L"Process", type) == 0) {
+				UINT32 新权限 = 0x1FFFFF;
+				info->name2.name1.GrantedAccessBits = 新权限;
+				KdPrint(("yjx:SYS 句柄=%llX, 权限=%X ,附加进程PID=%d +++++++>>>>>>>>>\n",
+					handle, info->name2.name1.GrantedAccessBits, HandleToPid(ProcessId, (HANDLE)handle)));
+			}
+			KdPrint(("\n"));
 		}
 		KdPrint(("驱动:SYS 句柄总数=%X   +++++++>>>>>>>>>\n\n", count));
-		//获取句柄类型信息
-		//POBJECT_TYPE objectType = GetHandleType((HANDLE)handle);
-		//if (objectType) {
-		//	UNICODE_STRING* typename = &objectType->Name;
-		//	KdPrint(("Handle: 0x%llX, Object Type: %S\n", handle, typename->Buffer));
-		//}
-
-		//PVOID object = NULL;
-		//POBJECT_TYPE objectType = NULL;
-		//PWCH type = NULL;
-		//// 获取对象指针
-		//NTSTATUS status = ObReferenceObjectByHandle((HANDLE)handle, 0, NULL, KernelMode, &object, NULL);
-		//if (NT_SUCCESS(status)) {
-		//	// 通过对象指针获取对象类型信息
-		//	objectType = *(POBJECT_TYPE*)((ULONG_PTR)object + sizeof(PVOID));
-		//	// 释放对象引用
-		//	ObDereferenceObject(object);
-		//	type = objectType->Name.Buffer;
-		//	if (type && _wcsicmp(L"Process", type) == 0) {
-		//		UINT32 新权限 = 0x1FFFFF;
-		//		info->name4.name1.GrantedAccessBits = 新权限;
-		//	}
-		//}
-		/*KdPrint(("yjx:SYS 句柄=%llX, info=%p,权限=%X 类型 =%S  +++++++>>>>>>>>>\n\n", handle, info,(info->name4).name1.GrantedAccessBits, type));*/
-
 	}
 	__except (1) {
 		KdPrint(("驱动:SYS 异常了  +++++++>>>>>>>>>\n\n"));
 	}
 }
-
-
-
-
-
-
-
-
-
-
 
 NTSTATUS IRP_通过进程遍历句柄(PIRP pirp) {
 	PIO_STACK_LOCATION irpStack;
