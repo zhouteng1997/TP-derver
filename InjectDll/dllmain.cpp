5490d460532b18cb7782550d30753e2f461076f0
周腾 <826409843@qq.com>
2024-08-05 14:33:28 +0800

    隐藏驱动注入dll，R3demo与被注入的dll

---
+++ InjectDll/dllmain.cpp
@@ -0,0 +1,89 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include <shlwapi.h>
+#include <tchar.h>
+#include <stdio.h>
+
+const TCHAR* GetCurrentProcessName(IN OUT TCHAR* exeName) {
+	GetModuleFileName(NULL, exeName, MAX_PATH);
+	PathStripPath(exeName);
+	return exeName;
+}
+
+DWORD __stdcall WorkThread(LPVOID lpram) {
+
+
+	TCHAR exeName[MAX_PATH];
+	GetCurrentProcessName(exeName);
+
+	char buf[1024];
+	sprintf_s(buf, "Inject OK \n");
+	OutputDebugStringA(buf);
+
+	if (_tcsicmp(exeName, _T("win32calc.exe")) != 0)
+	{
+		LoadLibraryA("C:\\Users\\admin\\Desktop\\工具\\InjectDl.dll");
+		MessageBox(NULL, exeName, _T("加载InjectDl.dll成功"), MB_ICONINFORMATION);
+		return 0;
+	}
+
+
+	return 0;
+
+
+	/*TCHAR exeName[MAX_PATH];
+	GetCurrentProcessName(exeName);
+
+	DWORD gamePid = GetGameProcessId();
+	DWORD currentPid = GetCurrentProcessId();
+
+	char buf[1024];
+	sprintf_s(buf, "新进程创建：  当前pid为%d \n", currentPid);
+	OutputDebugStringA(buf);
+
+
+	if (_tcsicmp(exeName, _T("aaaaaa.exe")) == 0) {
+		DWORD gamePid = GetGameProcessId();
+		DWORD currentPid = GetCurrentProcessId();
+		int num = 0;
+		while (gamePid != currentPid && num++ < 30) {
+			gamePid = GetGameProcessId();
+			currentPid = GetCurrentProcessId();
+			sprintf_s(buf, "比对： 当前pid为%d  游戏pid为%d \n", currentPid, gamePid);
+			OutputDebugStringA(buf);
+			Sleep(1000);
+		}
+		if (IsGameProcess()) {
+			sprintf_s(buf, "加载DLL： 当前pid为%d \n", currentPid);
+			OutputDebugStringA(buf);
+			LoadLibraryA("c:\\game64.dll");
+			MessageBox(0, exeName, _T("OK"), MB_OK);
+		}
+		return 0;
+	}
+	return 0;*/
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	{
+		HANDLE hTread = CreateThread(NULL, NULL, WorkThread, NULL, NULL, NULL);
+		if (hTread) {
+			CloseHandle(hTread);
+		}
+		break;
+	}
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
---
+++ injectDllDemo/injectDllDemo.cpp
@@ -0,0 +1,102 @@
+﻿// injectDllDemo.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <devioctl.h>
+#include <windows.h>
+
+
+#define 符号链接名64 L"\\??\\InjectDriver64"
+#define 符号链接名32 L"\\??\\InjectDriver32"
+
+#define InjectDll_X64 CTL_CODE(FILE_DEVICE_UNKNOWN, 0x900, METHOD_BUFFERED,FILE_ANY_ACCESS) //控制码测试
+
+PVOID MyReadFile(WCHAR* fileName, PULONG fileSize) {
+	HANDLE fileHandle = NULL;
+	DWORD read = 0;
+	PVOID fileBufPtr = NULL;
+
+	fileHandle = CreateFile(
+		fileName,
+		GENERIC_READ,
+		FILE_SHARE_READ,
+		NULL,
+		OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL
+	);
+
+	if (fileHandle == INVALID_HANDLE_VALUE) {
+		*fileSize = 0;
+		return NULL;
+	}
+
+	*fileSize = GetFileSize(fileHandle, NULL);
+	fileBufPtr = calloc(1, *fileSize);
+
+	if (!ReadFile(fileHandle, fileBufPtr, *fileSize, &read, NULL)) {
+		free(fileBufPtr);
+		fileBufPtr = NULL;
+		*fileSize = 0;
+	}
+
+	CloseHandle(fileHandle);
+	return fileBufPtr;
+}
+
+
+
+
+int main()
+{
+	//打开驱动设备
+	HANDLE DeviceHandle = CreateFileW(
+		符号链接名64,
+		GENERIC_READ | GENERIC_WRITE,
+		FILE_SHARE_READ | FILE_SHARE_WRITE, NULL,
+		OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL, NULL);
+	if (DeviceHandle == NULL) {
+		printf("驱动未加载");
+		return 0;
+	}
+
+	ULONG fileSize;
+	WCHAR fileName[] = L"InjectDll.dll";
+	PVOID dllx64Ptr = MyReadFile(fileName, &fileSize);
+	if (dllx64Ptr == NULL) {
+		return 0;
+	}
+
+	DWORD retSize = sizeof(INT64);
+	INT64 ret = 0;//输出缓冲区
+
+	BOOL result = DeviceIoControl(
+		DeviceHandle,//CreateFile打开驱动设备返回的句柄
+		InjectDll_X64,//控制码CTL_CODE
+		dllx64Ptr,//输入缓冲区指针
+		fileSize,//输入缓冲区大小
+		&ret,//返回缓冲区
+		sizeof(ret),//返回缓冲区大小
+		&retSize,//返回字节数
+		NULL);
+
+	if (DeviceHandle != NULL)
+		CloseHandle(DeviceHandle);
+	if (dllx64Ptr != NULL)
+		free(dllx64Ptr);
+
+	printf("已将dll传递给驱动");
+	getchar();
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
